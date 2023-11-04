f9cc817f1b4e63f37b3c4cf3b3818e27376077a6
Sam Roelants <sam@samroelants.com>
2023-11-04 21:36:39 +0100

    Add all remaining presets from the CPW perft page

--- perft/src/presets.rs
+++ perft/src/presets.rs
@@ -3,6 +3,9 @@ pub enum Preset {
     StartingPos,
     Kiwipete,
     Position3,
+    Position4,
+    Position5,
+    Position6,
 }
 
 
@@ -15,7 +18,7 @@ pub struct PerftPreset<'a> {
 }
 
 impl Preset {
-    const COUNT: usize = 3;
+    const COUNT: usize = 6;
 
     const PRESETS: [PerftPreset<'static>; Preset::COUNT] = [
         PerftPreset {
@@ -34,9 +37,30 @@ impl Preset {
 
         PerftPreset {
             name: "Position 3",
-            description: "An infamous board state to week out any edge cases",
+            description: "Position 3. Gave me a headache with EP checks",
             fen: "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
             expected: &[1, 14, 191, 2812, 43_238, 674_624, 1_103_0083, 178_633_661, 3_009_794_393],
+        },
+
+        PerftPreset {
+            name: "Position 4",
+            description: "An infamous board state to week out any edge cases",
+            fen: "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+            expected: &[1, 6, 264, 9467, 422_333, 15_833_292, 706_045_033 ],
+        },
+
+        PerftPreset {
+            name: "Position 5",
+            description: "Meant to give some trouble even in the very early moves",
+            fen: "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ",
+            expected: &[1, 44, 1486, 62_379, 2_103_487, 89_941_194 ],
+        },
+
+        PerftPreset {
+            name: "Position 6",
+            description: "",
+            fen: "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            expected: &[1, 46, 2_079, 89_890, 3_894_594, 164_075_551, 6_923_051_137, 287_188_994_746,  11_923_589_843_526, 490_154_852_788_714  ],
         },
     ];
 
