a609d35eb2866d722bcf63c5f849bc5b556ae143
Sam Roelants <sam@samroelants.com>
2023-11-19 14:26:43 +0100

    Add engine benchmarks

--- simbelmyne/src/cli/bench.rs
+++ simbelmyne/src/cli/bench.rs
@@ -1,108 +1,51 @@
-use crate::cli::{perft::run_perft, presets::{Preset, PerftPreset}};
-use anyhow::*;
-use chess::board::Board;
-use colored::*;
+use colored::Colorize;
 
-const BULK: bool = true;
+use crate::{position::Position, transpositions::TTable, search::SearchOpts};
 
-pub fn run_bench(depth: usize, fen: Option<String>, preset: Option<Preset>, all: bool) -> anyhow::Result<()> {
-    println!(
-        "🏃 {}",
-        "Running Perft test\n----------------------------"
-            .blue()
-            .italic()
-    );
-
-    if all {
-        for preset in Preset::all_presets() {
-            run_preset(preset, depth)?;
-        }
-    } else if let Some(fen) = fen {
-        run_fen(fen, depth)?;
-    } else if let Some(preset) = preset {
-        let preset = Preset::load_preset(preset);
-        run_preset(preset, depth)?;
-    }
-
-    Ok(())
-}
-
-fn run_preset(preset: &PerftPreset, depth: usize) -> anyhow::Result<()>{
-    let mut all_passed = true;
-    let board: Board = preset.fen.parse().unwrap();
-
-    println!("{}: {}", "Preset".green(), preset.name);
-    println!("{}: {}", "Description".green(), preset.description);
-    println!("{}: {}", "FEN".green(), preset.fen.italic());
-    println!("{}:\n\n{board}\n\n", "Board".green());
-
-    for depth in 0..=depth {
-        let result = run_perft::<BULK>(board, depth);
-        let is_match = match preset.expected.get(depth) {
-            Some(&expected) => result.nodes == expected,
-            None => true,
-        };
-
-        all_passed &= is_match;
-
-        print!("Depth {}: ", depth.to_string().blue());
-
-        let expected = preset
-            .expected
-            .get(depth)
-            .map(|&n| n.to_string())
-            .unwrap_or("".to_string())
-            .green();
-
-        print!("expected {expected:>12} ");
-
-        let found = if is_match {
-            result.nodes.to_string().green()
-        } else {
-            result.nodes.to_string().red()
-        };
-
-        print!("found {found:>12} ");
-
-        let mnps_str = format!("({:.3}Mnps)",result.mega_nps());
-        print!("in {:5}ms {:>15}", result.millis(), mnps_str);
-
-        if is_match {
-            print!("{:>3}","🎉");
-        } else {
-            print!("{:>3}", "💥");
-        }
-
-        println!("");
-
-    }
-        println!("\n\n");
-
-    if all_passed {
-        Ok(())
+use super::presets::Preset;
+pub fn run_bench(depth: usize, fen: Option<String>) {
+    if let Some(fen) = fen {
+        run_single(&fen, depth);
     } else {
-        return Err(anyhow!("Some perft results didn't match the expected value."));
+        for preset in Preset::all_presets() {
+            run_single(preset.fen, depth);
+        }
     }
 }
 
-fn run_fen(fen: String, depth: usize) -> anyhow::Result<()> {
-    let board: Board = fen.parse().unwrap();
+pub fn run_single(fen: &str, depth: usize) {
+    let board = fen.parse().unwrap();
+    let position = Position::new(board);
+    let mut tt = TTable::with_capacity(64);
+    let opts = SearchOpts::new();
 
-    println!("{}: {}", "FEN".green(), fen.italic());
-    println!("{}:\n\n{board}\n\n", "Board".green());
+    let search = position.search(depth, &mut tt, opts);
 
-    for depth in 0..=depth {
-        let result = run_perft::<BULK>(board, depth);
 
-        print!("Depth {}: ", depth.to_string().blue());
+    println!("{board}");
+    println!("{:15}: {}", "FEN".green(), fen);
+    println!("{:15}: {}", "Depth".green(), depth);
+    println!("---");
 
-        print!("found {:>12} ", result.nodes.to_string().green());
+    println!("{:15}: {}", "Best move".bright_cyan(), search.best_moves[0]);
+    println!("{:15}: {}", "Score".bright_cyan(), search.scores[0]);
+    println!("{:15}: {}", "Static eval".bright_cyan(), search.eval[0]);
 
-        print!("in {:5}ms ({:.3}Mnps)", result.millis(), result.mega_nps());
 
-        println!("");
-    }
-        println!("\n\n");
+    let nodes_visited: usize = search.nodes_visited.iter().sum();
+    println!("{:15}: {}", "Nodes visited".blue(), nodes_visited);
 
-    Ok(())
+    let leaf_nodes = search.nodes_visited[depth - 1];
+    println!("{:15}: {}", "Leaf nodes".blue(), leaf_nodes);
+
+    let beta_cutoffs: usize = search.beta_cutoffs.iter().sum();
+    println!("{:15}: {}", "Beta cutoffs".blue(), beta_cutoffs);
+
+    let time_spent = search.durations[0].as_millis();
+    println!("{:15}: {}ms", "Duration".red(), time_spent);
+
+    let knps = nodes_visited / time_spent as usize;
+    println!("{:15}: {}knps", "knps".red(), knps);
+
+    println!("\n");
 }
