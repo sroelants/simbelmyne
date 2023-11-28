d7ac171a185e23827de41610cd6437407581f2d0
Sam Roelants <sam@samroelants.com>
2023-11-29 00:42:31 +0100

    Add chessmatch tui

--- chessmatch/src/main.rs
+++ chessmatch/src/main.rs
@@ -1,13 +1,12 @@
 use std::path::Path;
-use chess::board::Board;
-use engine::{Config, Engine};
+use engine::Config;
 use serde_yaml;
-use tokio::select;
-use uci::UciEngineMessage;
+use tui::init_tui;
 
 mod uci;
 mod engine;
 mod tui;
+mod components;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
@@ -15,52 +14,7 @@ async fn main() -> anyhow::Result<()>{
     let config = std::fs::read_to_string(config_path).unwrap();
     let config: Config = serde_yaml::from_str(&config).unwrap();
 
-    let mut white = Engine::new(&config.white);
-    white.init().await;
+    init_tui(config).await.unwrap();
 
-    let mut black = Engine::new(&config.black);
-    black.init().await;
-
-    let mut board = Board::new();
-    let mut msg: Option<UciEngineMessage> = None;
-    let mut next_player = if board.current.is_white() {
-        &mut white
-    } else {
-        &mut black
-    };
-
-    next_player.set_pos(board).await;
-    next_player.go().await;
-
-    loop {
-        select! {
-            white_msg = (&mut white).read() => msg = Some(white_msg),
-            black_msg = (&mut black).read() => msg = Some(black_msg),
-        }
-
-        next_player = if board.current.is_white() {
-            &mut white
-        } else {
-            &mut black
-        };
-
-        if let Some(UciEngineMessage::BestMove(mv)) = msg {
-            board = board.play_move(mv);
-            println!("{}'s move: {mv}", board.current);
-            println!("{board}");
-            next_player.set_pos(board).await;
-            next_player.go().await;
-
-            if board.checkmate() {
-                println!("{} wins!", board.current.opp());
-                return Ok(())
-            }
-
-            if board.is_draw() {
-                println!("It's a draw!");
-                return Ok(())
-            }
-
-        }
-    }
+    Ok(())
 }
--- chessmatch/src/tui.rs
+++ chessmatch/src/tui.rs
@@ -1,13 +1,10 @@
-use std::path::Path;
-
 use chess::{piece::Color, board::Board, movegen::moves::Move};
-use crossterm::event::{EventStream, self, Event, KeyCode, KeyEvent};
-use tokio::stream::*;
-use ratatui::{Terminal, prelude::CrosstermBackend, Frame};
+use crossterm::event::{EventStream, Event, KeyCode};
+use ratatui::{Terminal, prelude::CrosstermBackend};
 use tokio::select;
 use tokio_stream::StreamExt;
 
-use crate::{engine::{Config, Engine}, uci::{UciEngineMessage, Info}};
+use crate::{engine::{Config, Engine}, uci::{UciEngineMessage, Info}, components::view};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum GameResult {
@@ -16,138 +13,163 @@ pub enum GameResult {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct EngineInfo {
-    name: String,
-    elo: Option<u32>,
-    depth: Option<u8>,
-    nodes: Option<u32>,
-    score: Option<i32>,
-    hashfull: Option<u32>,
-    nps: Option<u32>,
+pub enum GameMode {
+    Suite(Vec<Board>),
+    Infinite
 }
 
-impl EngineInfo {
-    pub fn update(&mut self, info: Info) {
-        self.depth = info.depth;
-        self.nodes = info.nodes;
-        self.score = info.score;
-        self.hashfull = info.hashfull;
-        self.nps = info.nps;
-    }
-}
-
-impl EngineInfo {
-    pub fn new(name: &str) -> Self {
-        Self {
-            name: name.to_owned(),
-            elo: None,
-            depth: None,
-            nodes: None,
-            score: None,
-            hashfull: None,
-            nps: None,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct State {
-    /// The current board state
-    board: Board,
+    /// The mode we're running the gauntlet in
+    pub mode: GameMode, 
 
-    /// Information on both engines, including name, elo, and current search information
-    players: [EngineInfo; Color::COUNT],
+    /// Count of how many games we've played so far
+    pub game_count: usize,
+
+    /// The current board state
+    pub board: Board,
+
+    /// The current game's starting position
+    pub initial_board: Board,
 
     /// The game result. None when the game is still ongoing
-    result: Option<GameResult>,
+    pub result: Option<GameResult>,
 
     /// If playing several games in a row, hold the result history so we can 
     /// show a summary
-    history: Vec<GameResult>,
+    pub history: Vec<GameResult>,
 
     /// Pause the play loop
-    paused: bool,
+    pub paused: bool,
 
     /// When true, exit on the next render loop iteration
-    should_quit: bool,
+    pub should_quit: bool,
+
+    pub engines: [Engine; Color::COUNT],
 }
 
 impl State {
-    pub fn new(board: Board, players: [EngineInfo; Color::COUNT]) -> Self{
+    pub fn new(mode: GameMode, white: Engine, black: Engine) -> Self{
+        let board = match &mode {
+            GameMode::Infinite => Board::new(),
+            GameMode::Suite(boards) => boards[0],
+        };
+
         Self {
+            mode,
+            game_count: 0,
             board,
-            players,
+            initial_board: board,
             result: None,
             history: Vec::new(),
             paused: false,
             should_quit: false,
+            engines: [white, black]
         }
     }
     
-    pub fn new_game(&mut self, board: Board) {
-        if let Some(result) = self.result {
-            self.history.push(result)
-        }
-
-        self.board = board;
-        self.result = None;
-        self.paused = false;
+    pub fn next_player(&mut self) -> &mut Engine {
+        &mut self.engines[self.board.current as usize]
     }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Message {
+    StartSearch,
     PlayMove(Move),
     UpdateInfo(Info),
     TogglePause,
-    NewGame(Board),
+    GameFinished(GameResult),
+    NewGame,
     Quit,
 }
 
-fn update(state: &mut State, message: Message) -> Option<Message> {
+async fn update(state: &mut State, message: Message) -> Option<Message> {
     use Message::*;
+
     match message {
-        PlayMove(mv) => {
-            state.board = state.board.play_move(mv);
+        StartSearch => {
+            let board = state.board;
+            state.next_player().set_pos(board).await;
+            state.next_player().go().await;
+            None
         },
+
+        PlayMove(mv) => {
+            std::thread::sleep(std::time::Duration::from_secs(2));
+            state.board = state.board.play_move(mv);
+
+            if state.board.checkmate() {
+                return Some(GameFinished(GameResult::Win(state.board.current.opp())));
+            }
+
+            if state.board.is_draw() {
+                return Some(GameFinished(GameResult::Draw));
+            }
+
+            Some(StartSearch)
+        },
+
+        GameFinished(result) => {
+            state.result = Some(result);
+            state.history.push(result);
+
+            match &state.mode {
+                GameMode::Infinite => Some(NewGame),
+
+                GameMode::Suite(boards) => {
+                    if state.game_count + 1 < boards.len() {
+                        Some(NewGame)
+                    } else {
+                        None
+                    }
+                }
+            }
+        }
 
         TogglePause => {
             state.paused = !state.paused;
+            None
         },
 
         Quit => {
             state.should_quit = true;
+            None
         },
 
-        NewGame(board) => {
-            state.new_game(board);
-        }
+        NewGame => {
+            state.result = None;
+            state.paused = false;
+            state.game_count += 1;
+
+
+            state.board = match &state.mode {
+                GameMode::Infinite => Board::new(),
+
+                // we checked that we're in bounds in GameFinished branch
+                GameMode::Suite(boards) => boards[state.game_count],
+            };
+
+            Some(StartSearch)
+        },
 
         UpdateInfo(info) => {
-            let current = state.board.current;
-            state.players[current as usize].depth = info.depth;
-            state.players[current as usize].depth = info.depth;
-            state.players[current as usize].depth = info.depth;
-            state.players[current as usize].depth = info.depth;
-            state.players[current as usize].depth = info.depth;
-
+            state.engines[state.board.current as usize].update_info(info);
+            None
         }
     }
-
-    None
 }
 
-fn view(state: &mut State, f: &mut Frame) {
-    todo!()
-}
-
-fn initialize_panic_handler() {
-    let original_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |panic_info| {
-        crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen).unwrap();
-        crossterm::terminal::disable_raw_mode().unwrap();
-        original_hook(panic_info);
-    }));
+async fn handle_event(stream: &mut EventStream) -> Message {
+    loop {
+        if let Some(Ok(Event::Key(key))) = stream.next().await {
+            match key.code {
+                KeyCode::Char('q') => return Message::Quit,
+                KeyCode::Char(' ') => return Message::TogglePause,
+                _ => {},
+            }
+        }
+    }
 }
 
 impl Engine {
@@ -164,6 +186,15 @@ impl Engine {
     }
 }
 
+fn initialize_panic_handler() {
+    let original_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen).unwrap();
+        crossterm::terminal::disable_raw_mode().unwrap();
+        original_hook(panic_info);
+    }));
+}
+
 pub async fn init_tui(config: Config) -> anyhow::Result<()> {
     initialize_panic_handler();
 
@@ -173,43 +204,50 @@ pub async fn init_tui(config: Config) -> anyhow::Result<()> {
 
     let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
 
-
     // Set up state
     let mut white = Engine::new(&config.white);
     let mut black = Engine::new(&config.black);
     white.init().await;
     black.init().await;
 
-    let mut board = Board::new();
-    let mut next_player = if board.current.is_white() { &mut white } else { &mut black };
+    let board = Board::new();
 
-    let players = [
-        EngineInfo::new(&config.white.name), 
-        EngineInfo::new(&config.black.name)
-    ];
+    let mode = match config.positions {
+        Some(fens) => {
+            let boards: Vec<Board> = fens.iter()
+                .map(|fen| fen.parse().unwrap())
+                .collect();
+            GameMode::Suite(boards)
+        },
+        None => GameMode::Infinite
+    };
 
-    let mut state = State::new(board, players);
+    let mut state = State::new(mode, white, black);
 
     let mut event_stream = EventStream::new();
+
+    state.next_player().set_pos(board).await;
+    state.next_player().go().await;
 
     // Start loop
     loop {
         let mut msg = None;
 
-        // Listen for engine updates or user events, whichever comes in first
-        select! {
-            engine_msg = next_player.read_message() => msg = Some(engine_msg),
-            event_msg = handle_event(&mut event_stream) => msg = Some(event_msg)
-        }
-
-        if let Some(msg) = msg {
-            update(&mut state, msg);
-        }
-
         // Render the current view
         terminal.draw(|f| {
             view(&mut state, f);
         })?;
+
+        // Listen for engine updates or user events, whichever comes in first
+        // TODO: How do I make this work with 'state.paused'?
+        select! {
+            engine_msg = state.next_player().read_message() => msg = Some(engine_msg),
+            event_msg = handle_event(&mut event_stream) => msg = Some(event_msg)
+        }
+
+        while let Some(next_message) = msg {
+            msg = update(&mut state, next_message).await;
+        }
 
         // Exit loop if quit flag is set
         if state.should_quit {
@@ -221,16 +259,4 @@ pub async fn init_tui(config: Config) -> anyhow::Result<()> {
     crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen)?;
     crossterm::terminal::disable_raw_mode()?;
     Ok(())
-}
-
-async fn handle_event(stream: &mut EventStream) -> Message {
-    loop {
-        if let Some(Ok(Event::Key(key))) = stream.next().await {
-            match key.code {
-                KeyCode::Char('q') => return Message::Quit,
-                KeyCode::Char(' ') => return Message::TogglePause,
-                _ => {},
-            }
-        }
-    }
 }
