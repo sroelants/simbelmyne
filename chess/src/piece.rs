f9fc67d28916d9778bc8e5dcc567406fdadf5789
Sam Roelants <15130108+sroelants@users.noreply.github.com>
2023-12-10 17:02:28 +0100

    Chess/piece cleanup (#93)
    * Remove unused helpers
    
    * Simplify printing/parsing for piecetype and color
    
    * Be less clever in piece creation
    
    * Add comments

--- chess/src/piece.rs
+++ chess/src/piece.rs
@@ -1,5 +1,98 @@
 use std::{fmt::Display, str::FromStr, ops::Not};
 use anyhow::anyhow;
+use PieceType::*;
+use Piece::*;
+use Color::*;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Piece {
+    WP, BP, WN, BN, WB, BB, WR, BR, WQ, BQ, WK, BK
+}
+
+#[allow(dead_code)]
+impl Piece {
+    pub const COUNT: usize = 12;
+
+    pub fn new(ptype: PieceType, color: Color) -> Self {
+        match (color, ptype) {
+            (White, Pawn)   => WP,
+            (White, Knight) => WN,
+            (White, Bishop) => WB,
+            (White, Rook)   => WR,
+            (White, Queen)  => WQ,
+            (White, King)   => WK,
+
+            (Black, Pawn)   => BP,
+            (Black, Knight) => BN,
+            (Black, Bishop) => BB,
+            (Black, Rook)   => BR,
+            (Black, Queen)  => BQ,
+            (Black, King)   => BK
+        }
+    }
+
+    /// Get the color of the piece
+    pub fn color(self) -> Color {
+        if self as usize & 1 == 1 { Color::White } else { Color::Black }
+    }
+
+    /// Get the piece type
+    pub fn piece_type(self) -> PieceType {
+        match self {
+            WP | BP => Pawn,
+            WN | BN => Knight,
+            WB | BB => Bishop,
+            WR | BR => Rook,
+            WQ | BQ => Queen,
+            WK | BK => King,
+        }
+    }
+
+    /// Check whether the piece is a pawn
+    pub fn is_pawn(&self) -> bool {
+        self.piece_type() == PieceType::Pawn
+    }
+
+    /// Check whether the piece is a knight
+    pub fn is_knight(&self) -> bool {
+        self.piece_type() == PieceType::Knight
+    }
+
+    /// Check whether the piece is a bishop
+    pub fn is_bishop(&self) -> bool {
+        self.piece_type() == PieceType::Bishop
+    }
+
+    /// Check whether the piece is a rook
+    pub fn is_rook(&self) -> bool {
+        self.piece_type() == PieceType::Rook
+    }
+
+    /// Check whether the piece is a queen
+    pub fn is_queen(&self) -> bool {
+        self.piece_type() == PieceType::Queen
+    }
+
+    /// Check whether the piece is a king
+    pub fn is_king(&self) -> bool {
+        self.piece_type() == PieceType::King
+    }
+
+    /// Check whether the piece is a slider
+    pub fn is_slider(&self) -> bool {
+        self.is_rook() || self.is_bishop() || self.is_queen()
+    }
+
+    /// Check whether the piece is a horizontal/vertical slider (rook or queen)
+    pub fn is_hv_slider(&self) -> bool {
+        self.is_rook() || self.is_queen()
+    }
+
+    /// Check whether the piece is a diagonal slider (bishop or queen)
+    pub fn is_diag_slider(&self) -> bool {
+        self.is_bishop() || self.is_queen()
+    }
+}
 
 #[repr(u8)]
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -14,38 +107,6 @@ pub enum PieceType {
 
 impl PieceType {
     pub const COUNT: usize = 6;
-    pub const ALL: [Self; Self::COUNT] = [
-        PieceType::Pawn,
-        PieceType::Knight,
-        PieceType::Bishop,
-        PieceType::Rook,
-        PieceType::Queen,
-        PieceType::King 
-    ];
-
-    pub fn is_pawn(&self) -> bool {
-        *self == PieceType::Pawn
-    }
-
-    pub fn is_rook(&self) -> bool {
-        *self == PieceType::Rook
-    }
-
-    pub fn is_knight(&self) -> bool {
-        *self == PieceType::Knight
-    }
-
-    pub fn is_bishop(&self) -> bool {
-        *self == PieceType::Bishop
-    }
-
-    pub fn is_queen(&self) -> bool {
-        *self == PieceType::Queen
-    }
-
-    pub fn is_king(&self) -> bool {
-        *self == PieceType::King
-    }
 }
 
 #[repr(u8)]
@@ -58,135 +119,28 @@ pub enum Color {
 impl Color {
     pub const COUNT: usize = 2;
 
-    pub fn opp(&self) -> Self {
-        !*self
+    /// Get the opposite color
+    pub fn opp(self) -> Self {
+        !self
     }
 
-    pub fn is_white(&self) -> bool {
-        *self == Color::White
+    /// Check whether the color is white
+    pub fn is_white(self) -> bool {
+        self == White
     }
 
-    pub fn is_black(&self) -> bool {
-        *self == Color::Black
+    /// Check whether the color is black
+    pub fn is_black(self) -> bool {
+        self == Black
     }
 
-    pub fn to_fen(&self) -> String {
-        if self.is_white() {
-            String::from("w")
-        } else {
-            String::from("b")
-        }
-    }
 }
 
-impl Display for Color {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Color::White => write!(f, "White")?,
-            Color::Black => write!(f, "Black")?,
-        }
-        Ok(())
-    }
-}
-
-impl FromStr for Color {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> anyhow::Result<Self> {
-        match s {
-            "w" | "W" | "white" | "White" => Ok(Color::White),
-            "b" | "B" | "black" | "Black" => Ok(Color::Black),
-            _ => Err(anyhow!("Not a valid color string"))?,
-        }
-    }
-}
-impl Not for Color {
-    type Output = Color;
-
-    fn not(self) -> Self::Output {
-        match self {
-            Color::White => Color::Black,
-            Color::Black => Color::White,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum Piece {
-    WP, BP, WN, BN, WB, BB, WR, BR, WQ, BQ, WK, BK
-}
-
-#[allow(dead_code)]
-impl Piece {
-    pub const COUNT: usize = 12;
-
-    pub const ALL: [Self; Self::COUNT] = [
-        Self::WP, 
-        Self::BP, 
-        Self::WN, 
-        Self::BN, 
-        Self::WB, 
-        Self::BB, 
-        Self::WR, 
-        Self::BR, 
-        Self::WQ, 
-        Self::BQ, 
-        Self::WK, 
-        Self::BK
-    ];
-
-    pub fn new(ptype: PieceType, color: Color) -> Self {
-        if color.is_white() {
-            Self::ALL[2 * (ptype as usize)]
-        } else {
-            Self::ALL[2 * (ptype as usize) + 1]
-        }
-    }
-
-    pub fn color(self) -> Color {
-        if self as usize % 2 == 0 { Color::White } else { Color::Black }
-    }
-
-    pub fn piece_type(self) -> PieceType {
-        PieceType::ALL[self as usize / 2]
-    }
-
-    pub fn is_pawn(&self) -> bool {
-        self.piece_type() == PieceType::Pawn
-    }
-
-    pub fn is_rook(&self) -> bool {
-        self.piece_type() == PieceType::Rook
-    }
-
-    pub fn is_knight(&self) -> bool {
-        self.piece_type() == PieceType::Knight
-    }
-
-    pub fn is_bishop(&self) -> bool {
-        self.piece_type() == PieceType::Bishop
-    }
-
-    pub fn is_queen(&self) -> bool {
-        self.piece_type() == PieceType::Queen
-    }
-
-    pub fn is_king(&self) -> bool {
-        self.piece_type() == PieceType::King
-    }
-
-    pub fn is_hv_slider(&self) -> bool {
-        self.is_rook() || self.is_queen()
-    }
-
-    pub fn is_diag_slider(&self) -> bool {
-        self.is_bishop() || self.is_queen()
-    }
-
-    pub fn is_slider(&self) -> bool {
-        self.is_rook() || self.is_bishop() || self.is_queen()
-    }
-}
+////////////////////////////////////////////////////////////////////////////////
+//
+// Utility Traits
+//
+////////////////////////////////////////////////////////////////////////////////
 
 impl Display for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -210,3 +164,36 @@ impl Display for Piece {
         write!(f, "{piece}")
     }
 }
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            White => write!(f, "w")?,
+            Black => write!(f, "b")?,
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for Color {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        match s {
+            "w" => Ok(White),
+            "b" => Ok(Black),
+            _ => Err(anyhow!("Not a valid color string"))?,
+        }
+    }
+}
+impl Not for Color {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            White => Black,
+            Black => White,
+        }
+    }
+}
+
