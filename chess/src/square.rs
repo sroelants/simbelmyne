ffc6af1fddd57c54fbdda6d35833aa8af4deffd7
Sam Roelants <sam@samroelants.com>
2023-12-10 10:57:35 +0100

    Export `Square::RANKS` for more easily iterating over ranks and files

--- chess/src/square.rs
+++ chess/src/square.rs
@@ -32,6 +32,11 @@ use Square::*;
 
 use crate::piece::Color;
 impl Square {
+    pub const COUNT: usize = 64;
+    pub const W_PAWN_RANK: usize = 1;
+    pub const B_PAWN_RANK: usize = 6;
+    pub const W_DPUSH_RANK: usize = 3;
+    pub const B_DPUSH_RANK: usize = 4;
     pub const ALL: [Square; Square::COUNT] = [
         A1, B1, C1, D1, E1, F1, G1, H1, 
         A2, B2, C2, D2, E2, F2, G2, H2, 
@@ -42,13 +47,6 @@ impl Square {
         A7, B7, C7, D7, E7, F7, G7, H7, 
         A8, B8, C8, D8, E8, F8, G8, H8,
     ];
-
-    pub const COUNT: usize = 64;
-    pub const W_PAWN_RANK: usize = 1;
-    pub const B_PAWN_RANK: usize = 6;
-    pub const W_DPUSH_RANK: usize = 3;
-    pub const B_DPUSH_RANK: usize = 4;
-
     pub fn new(rank: usize, file: usize) -> Square {
         Square::ALL[rank * 8 + file]
     }
@@ -72,6 +70,16 @@ impl Square {
     pub fn to_alg(&self) -> &'static str {
         SQUARE_NAMES[*self as usize]
     }
+    pub const RANKS: [[Square; 8]; 8] = [
+        [A1, B1, C1, D1, E1, F1, G1, H1], 
+        [A2, B2, C2, D2, E2, F2, G2, H2], 
+        [A3, B3, C3, D3, E3, F3, G3, H3], 
+        [A4, B4, C4, D4, E4, F4, G4, H4], 
+        [A5, B5, C5, D5, E5, F5, G5, H5], 
+        [A6, B6, C6, D6, E6, F6, G6, H6], 
+        [A7, B7, C7, D7, E7, F7, G7, H7], 
+        [A8, B8, C8, D8, E8, F8, G8, H8],
+    ];
 
     pub fn rank(&self) -> usize {
         (*self as usize) / 8
