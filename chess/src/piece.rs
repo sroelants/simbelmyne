199c866693dd7281b51a50d070194de9f249ce00
Sam Roelants <sam@samroelants.com>
2023-12-10 16:51:35 +0100

    Add comments

--- chess/src/piece.rs
+++ chess/src/piece.rs
@@ -13,10 +13,6 @@ pub enum Piece {
 impl Piece {
     pub const COUNT: usize = 12;
 
-    pub const ALL: [Self; Self::COUNT] = [
-        WP, BP, WN, BN, WB, BB, WR, BR, WQ, BQ, WK, BK
-    ];
-
     pub fn new(ptype: PieceType, color: Color) -> Self {
         match (color, ptype) {
             (White, Pawn)   => WP,
@@ -35,50 +31,116 @@ impl Piece {
         }
     }
 
+    /// Get the color of the piece
     pub fn color(self) -> Color {
         if self as usize & 1 == 1 { Color::White } else { Color::Black }
     }
 
+    /// Get the piece type
     pub fn piece_type(self) -> PieceType {
-        PieceType::ALL[self as usize / 2]
+        match self {
+            WP | BP => Pawn,
+            WN | BN => Knight,
+            WB | BB => Bishop,
+            WR | BR => Rook,
+            WQ | BQ => Queen,
+            WK | BK => King,
+        }
     }
 
+    /// Check whether the piece is a pawn
     pub fn is_pawn(&self) -> bool {
         self.piece_type() == PieceType::Pawn
     }
 
-    pub fn is_rook(&self) -> bool {
-        self.piece_type() == PieceType::Rook
-    }
-
+    /// Check whether the piece is a knight
     pub fn is_knight(&self) -> bool {
         self.piece_type() == PieceType::Knight
     }
 
+    /// Check whether the piece is a bishop
     pub fn is_bishop(&self) -> bool {
         self.piece_type() == PieceType::Bishop
     }
 
+    /// Check whether the piece is a rook
+    pub fn is_rook(&self) -> bool {
+        self.piece_type() == PieceType::Rook
+    }
+
+    /// Check whether the piece is a queen
     pub fn is_queen(&self) -> bool {
         self.piece_type() == PieceType::Queen
     }
 
+    /// Check whether the piece is a king
     pub fn is_king(&self) -> bool {
         self.piece_type() == PieceType::King
     }
 
+    /// Check whether the piece is a slider
+    pub fn is_slider(&self) -> bool {
+        self.is_rook() || self.is_bishop() || self.is_queen()
+    }
+
+    /// Check whether the piece is a horizontal/vertical slider (rook or queen)
     pub fn is_hv_slider(&self) -> bool {
         self.is_rook() || self.is_queen()
     }
 
+    /// Check whether the piece is a diagonal slider (bishop or queen)
     pub fn is_diag_slider(&self) -> bool {
         self.is_bishop() || self.is_queen()
     }
-
-    pub fn is_slider(&self) -> bool {
-        self.is_rook() || self.is_bishop() || self.is_queen()
-    }
 }
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum PieceType {
+    Pawn = 0,
+    Knight = 1,
+    Bishop = 2,
+    Rook = 3,
+    Queen = 4,
+    King = 5,
+}
+
+impl PieceType {
+    pub const COUNT: usize = 6;
+}
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Color {
+    White = 0,
+    Black = 1,
+}
+
+impl Color {
+    pub const COUNT: usize = 2;
+
+    /// Get the opposite color
+    pub fn opp(self) -> Self {
+        !self
+    }
+
+    /// Check whether the color is white
+    pub fn is_white(self) -> bool {
+        self == White
+    }
+
+    /// Check whether the color is black
+    pub fn is_black(self) -> bool {
+        self == Black
+    }
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Utility Traits
+//
+////////////////////////////////////////////////////////////////////////////////
 
 impl Display for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -101,47 +163,6 @@ impl Display for Piece {
 
         write!(f, "{piece}")
     }
-}
-
-#[repr(u8)]
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum PieceType {
-    Pawn = 0,
-    Knight = 1,
-    Bishop = 2,
-    Rook = 3,
-    Queen = 4,
-    King = 5,
-}
-
-impl PieceType {
-    pub const COUNT: usize = 6;
-    pub const ALL: [Self; Self::COUNT] = 
-        [ Pawn, Knight, Bishop, Rook, Queen, King ];
-}
-
-#[repr(u8)]
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum Color {
-    White = 0,
-    Black = 1,
-}
-
-impl Color {
-    pub const COUNT: usize = 2;
-
-    pub fn opp(&self) -> Self {
-        !*self
-    }
-
-    pub fn is_white(&self) -> bool {
-        *self == White
-    }
-
-    pub fn is_black(&self) -> bool {
-        *self == Black
-    }
-
 }
 
 impl Display for Color {
