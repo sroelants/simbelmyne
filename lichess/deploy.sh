28e5d789025088835048e571360a1bc01976d298
Sam Roelants <sam@samroelants.com>
2024-06-01 19:14:21 +0200

    Tweak lichess deploy script

--- lichess/deploy.sh
+++ lichess/deploy.sh
@@ -5,4 +5,5 @@ docker build -t lichess-bot .
 
 # Replace running lichess-bot container with newly built one
 docker kill lichess-bot
+docker rm lichess-bot
 docker run -d --name=lichess-bot --env-file ~/.env lichess-bot:latest 
