be192814ad4c9f158562a6540e27485303cadec2
Sam Roelants <sam@samroelants.com>
2025-03-21 01:04:45 +0100

    Use history to determine good tacticals
    bench 7217152

--- simbelmyne/src/move_picker.rs
+++ simbelmyne/src/move_picker.rs
@@ -182,9 +182,13 @@ impl<'pos> MovePicker<'pos> {
         return Some(best_move);
     }
 
-    fn is_good_tactical(&self, mv: Move) -> bool {
+    fn is_good_tactical(&self, mv: Move, history: &History) -> bool {
         use PieceType::*;
-        if mv.is_capture() {
+        let hist_score = history.get_hist_score(mv, &self.position.board);
+
+        if hist_score >= 10000 {
+            true
+        } else if mv.is_capture() {
             self.position.board.see(mv, 0)
         } else {
             mv.get_promo_type().is_some_and(|pt| pt == Queen)
@@ -350,7 +354,7 @@ impl<'a> MovePicker<'a> {
             while self.index < self.bad_tactical_index {
                 let tactical = self.partial_sort(self.index, self.bad_tactical_index);
 
-                if self.is_good_tactical(tactical.unwrap()) {
+                if self.is_good_tactical(tactical.unwrap(), history) {
                     self.index += 1;
                     return tactical;
                 } else {
