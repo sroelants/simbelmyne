eed7ce20733dc663ba8222d4ccf7b9d32b3869bf
Sam Roelants <sam@samroelants.com>
2025-04-11 11:32:29 +0200

    Centralize corrhist updates/correction
    Also make contributions tuneable
    
    bench 3745792

--- engine/src/history_tables/mod.rs
+++ engine/src/history_tables/mod.rs
@@ -1,17 +1,22 @@
 use arrayvec::ArrayVec;
-use chess::{board::Board, movegen::moves::Move, piece::{Color, PieceType}, square::Square};
-use corrhist::{Hash, CorrHistEntry, CORRHIST_SIZE};
+use chess::{
+    board::Board,
+    movegen::moves::Move,
+    piece::{Color, PieceType},
+    square::Square,
+};
+use corrhist::{CorrHistEntry, Hash, CORRHIST_SIZE};
 use history::{Butterfly, HistoryIndex, HistoryScore};
 use killers::Killers;
 use threats::{ThreatIndex, Threats};
 
-use crate::{search::params::MAX_DEPTH, zobrist::ZHash};
+use crate::{evaluate::Score, position::Position, search::params::{cont_corr_weight, material_corr_weight, minor_corr_weight, nonpawn_corr_weight, pawn_corr_weight, MAX_DEPTH}, zobrist::ZHash};
 
+pub mod corrhist;
 pub mod history;
-pub mod threats;
 pub mod killers;
 pub mod pv;
-pub mod corrhist;
+pub mod threats;
 
 #[derive(Debug)]
 pub struct History {
@@ -30,8 +35,8 @@ pub struct History {
 impl History {
     pub fn boxed() -> Box<Self> {
         #![allow(clippy::cast_ptr_alignment)]
-        // SAFETY: we're allocating a zeroed block of memory, and then casting 
-        // it to a Box<Self>. This is fine! 
+        // SAFETY: we're allocating a zeroed block of memory, and then casting
+        // it to a Box<Self>. This is fine!
         // [[HistoryTable; Square::COUNT]; Piece::COUNT] is just a bunch of i16s
         // in disguise, which are fine to zero-out.
         unsafe {
@@ -46,7 +51,7 @@ impl History {
 
     // History indices
     pub fn push_mv(&mut self, mv: Move, board: &Board) {
-            self.indices.push(HistoryIndex::new(board, mv));
+        self.indices.push(HistoryIndex::new(board, mv));
     }
 
     pub fn push_null_mv(&mut self) {
@@ -66,12 +71,7 @@ impl History {
     }
 
     // Update History tables
-    pub fn add_hist_bonus(
-        &mut self, 
-        mv: Move, 
-        board: &Board, 
-        bonus: HistoryScore
-    ) {
+    pub fn add_hist_bonus(&mut self, mv: Move, board: &Board, bonus: HistoryScore) {
         let idx = HistoryIndex::new(board, mv);
 
         if mv.is_tactical() {
@@ -82,27 +82,34 @@ impl History {
             };
 
             self.tact_hist[victim][idx] += bonus;
-        } 
-
-        else {
+        } else {
             let threat_idx = ThreatIndex::new(board.threats, mv);
             self.main_hist[threat_idx][idx] += bonus;
 
-            if let Some(oneply) = self.indices.len()
+            if let Some(oneply) = self
+                .indices
+                .len()
                 .checked_sub(1)
-                .map(|ply| self.indices[ply]) {
+                .map(|ply| self.indices[ply])
+            {
                 self.cont_hist[oneply][idx] += bonus;
             }
 
-            if let Some(twoply) = self.indices.len()
+            if let Some(twoply) = self
+                .indices
+                .len()
                 .checked_sub(2)
-                .map(|ply| self.indices[ply]) {
+                .map(|ply| self.indices[ply])
+            {
                 self.cont_hist[twoply][idx] += bonus;
             }
 
-            if let Some(fourply) = self.indices.len()
+            if let Some(fourply) = self
+                .indices
+                .len()
                 .checked_sub(4)
-                .map(|ply| self.indices[ply]) {
+                .map(|ply| self.indices[ply])
+            {
                 self.cont_hist[fourply][idx] += bonus;
             }
         }
@@ -123,21 +130,30 @@ impl History {
             let threat_idx = ThreatIndex::new(board.threats, mv);
             let mut total = i32::from(self.main_hist[threat_idx][idx]);
 
-            if let Some(oneply) = self.indices.len()
+            if let Some(oneply) = self
+                .indices
+                .len()
                 .checked_sub(1)
-                .map(|ply| self.indices[ply]) {
+                .map(|ply| self.indices[ply])
+            {
                 total += i32::from(self.cont_hist[oneply][idx]);
             }
 
-            if let Some(twoply) = self.indices.len()
+            if let Some(twoply) = self
+                .indices
+                .len()
                 .checked_sub(2)
-                .map(|ply| self.indices[ply]) {
+                .map(|ply| self.indices[ply])
+            {
                 total += i32::from(self.cont_hist[twoply][idx]);
             }
 
-            if let Some(fourply) = self.indices.len()
+            if let Some(fourply) = self
+                .indices
+                .len()
                 .checked_sub(4)
-                .map(|ply| self.indices[ply]) {
+                .map(|ply| self.indices[ply])
+            {
                 total += i32::from(self.cont_hist[fourply][idx]);
             }
 
@@ -183,5 +199,61 @@ impl History {
 
     pub fn get_nodes(&self, mv: Move) -> u32 {
         self.node_counts[mv.src()][mv.tgt()]
+    }
+
+    // Corrhist
+    pub fn correct_eval(&self, pos: &Position, ply: usize, eval: Score) -> Score {
+        use Color::*;
+        let us = pos.board.current;
+
+        let pawn_correction = self.corr_hist[us][pos.pawn_hash].corr();
+        let w_nonpawn_correction = self.corr_hist[us][pos.nonpawn_hashes[White]].corr();
+        let b_nonpawn_correction = self.corr_hist[us][pos.nonpawn_hashes[Black]].corr();
+        let material_correction = self.corr_hist[us][pos.material_hash].corr();
+        let minor_correction = self.corr_hist[us][pos.minor_hash].corr();
+        let cont_correction = self
+            .indices
+            .get(ply - 2)
+            .map(|idx| self.contcorr_hist[*idx].corr())
+            .unwrap_or_default();
+
+        let correction =
+              pawn_corr_weight()     * pawn_correction
+            + nonpawn_corr_weight()  * w_nonpawn_correction
+            + nonpawn_corr_weight()  * b_nonpawn_correction
+            + material_corr_weight() * material_correction
+            + minor_corr_weight()    * minor_correction
+            + cont_corr_weight()     * cont_correction;
+
+        eval + correction
+    }
+    pub fn update_corrhist(
+        &mut self,
+        pos: &Position,
+        ply: usize,
+        depth: usize,
+        score: Score,
+        eval: Score,
+    ) {
+        use Color::*;
+        let us = pos.board.current;
+
+        // Update the pawn corrhist
+        self.corr_hist[us][pos.pawn_hash].update(score, eval, depth);
+
+        // Update the non-pawn corrhist
+        self.corr_hist[us][pos.nonpawn_hashes[White]].update(score, eval, depth);
+        self.corr_hist[us][pos.nonpawn_hashes[Black]].update(score, eval, depth);
+
+        // Update the material corrhist
+        self.corr_hist[us][pos.material_hash].update(score, eval, depth);
+
+        // Update the minor corrhist
+        self.corr_hist[us][pos.minor_hash].update(score, eval, depth);
+
+        // Update the cont corrhist
+        if let Some(idx) = self.indices.get(ply - 2) {
+            self.contcorr_hist[*idx].update(score, eval, depth);
+        }
     }
 }
