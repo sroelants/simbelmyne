12d11ec4bb1e1bea33ab9c3a2015077108dc207d
Sam Roelants <sam@samroelants.com>
2025-08-06 13:31:01 +0200

    Use original values
    bench 4098048

--- engine/src/search/negamax.rs
+++ engine/src/search/negamax.rs
@@ -648,7 +648,7 @@ impl<'a> SearchRunner<'a> {
         // If score > alpha, but we were searching at reduced depth,
         // do a full-depth, zero-window search
         if score > alpha && reduced < new_depth {
-          let ext = (score > best_score + 28 + 1 * new_depth as i32) as i16;
+          let ext = (score > best_score + 38 + 2 * new_depth as i32) as i16;
           let red = (score < best_score + 8) as i16;
           let new_depth = new_depth + ext - red;
 
