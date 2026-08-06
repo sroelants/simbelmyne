64bd8f92398d0cab508874badf8e3a848f8b7b7f
Sam Roelants <sam@samroelants.com>
2026-08-06 08:25:31 +0200

    Tweak condition
    bench 3325952

--- engine/src/search/negamax.rs
+++ engine/src/search/negamax.rs
@@ -225,9 +225,10 @@ impl<'a> SearchRunner<'a> {
     if !PV
       && !in_check
       && excluded.is_none()
-      && depth <= 1
+      && depth <= 3
       && static_eval + 500 * depth as Score <= alpha
       && tt_move.is_none_or(|mv| mv.is_tactical())
+      && tt_entry.is_none_or(|entry| entry.get_type() != NodeType::Lower)
       && alpha < 2000
     {
       let score =
