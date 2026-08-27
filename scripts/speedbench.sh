e5725d9dfee725ed0dbcfad846989aafb4ded4b8
Sam Roelants <sam@samroelants.com>
2026-08-27 13:04:08 +0200

    Skip building new version if binary already exists
    also do better error detection
    
    bench 3358720

--- scripts/speedbench.sh
+++ scripts/speedbench.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-BLUE="\e[34m"
+RED="\e[31m"
 GREEN="\e[32m"
+BLUE="\e[34m"
 NORM="\e[0m"
 
 BASE=$1
@@ -10,24 +11,42 @@ DEV=$2
 RUNS=100
 NODES=10000
 
+
 # Check out a commit and build the binary
 function build_version() {
-  echo -en "Building Simbelmyne branch $BLUE$1$NORM..."
   git checkout "$1" > /dev/null 2> /dev/null
 
-  RUSTFLAGS="-Awarnings -Ctarget-cpu=native" cargo build --release -q
+  if [ ! $? -eq 0 ]; then
+    echo -e "${RED}[ERR]:${NORM} Failed to check out branch $BLUE$1$NORM."
+    echo -en "Output: "
+    git checkout "$1" > /dev/null
+    exit 1
+  fi
 
-  echo -e "${GREEN}Done$NORM"
-  cp "target/release/simbelmyne" "/tmp/simbelmyne-$1"
+  DEST="/tmp/simbelmyne-$(git rev-parse --short HEAD)"
+
+  if [ ! -e "$DEST" ]; then
+    echo -en "Building Simbelmyne branch $BLUE$1$NORM..." >&2
+    RUSTFLAGS="-Awarnings -Ctarget-cpu=native" cargo build --release -q
+    echo -e "${GREEN}Done$NORM" >&2
+    cp "target/release/simbelmyne" "$DEST"
+  else
+    echo -e "Found cached file for Simbelmyne $BLUE$1$NORM, skipping build." >&2
+  fi
+
   git checkout - > /dev/null 2>/dev/null
+
+  # Return the binary path
+  echo "$DEST"
 }
 
-build_version "$BASE"
-build_version "$DEV"
+BASE_PATH=$(build_version "$BASE")
+DEV_PATH=$(build_version "$DEV")
 
 hyperfine \
   --runs "$RUNS" \
   --command-name "$BASE" \
-  "/tmp/simbelmyne-$BASE bench -n $NODES" \
+  "$BASE_PATH bench -n $NODES" \
   --command-name "$DEV" \
-  "/tmp/simbelmyne-$DEV bench -n $NODES"
+  "$DEV_PATH bench -n $NODES"
+
